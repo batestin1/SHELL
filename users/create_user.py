29a3211e9e73b89ed0dcfd54b20d00b32bcc0fb2
batestin1 <mayconcipriano@gmail.com>
2022-03-04 14:20:45 -0300

    v3 - 20220304

--- users/create_user.py
+++ users/create_user.py
@@ -16,17 +16,19 @@ import sys
 nom = sys.argv[1:]
 
 def create_user(nom):
+    
     with open("users.txt", "a+", encoding='UTF-8') as output:
-        global list    
-        sep = ''
-        fullName = sep.join(nom)
+        global list
+        global nameFull
+        nameFull = " ".join(nom)
+        fullName = ''.join(nom)
         list = list(fullName)
         sort = random.sample(list, len(list))
-        user_1 = sep.join(sort[:4])
-        user_2 = sep.join(list[:3])
+        user_1 = ''.join(sort[:4])
+        user_2 = ''.join(list[:3])
         user_final = user_2+user_1
         user_final = user_final.lower()
-        output.write(f"""{user_final}:{random.randint(111,999)}:{random.randint(1111,9999)}:{random.randint(111,999)}:{user_final.capitalize()},Brasil,{random.randint(111,999)}-{random.randint(111,999)}:/home/{user_final}:/bin/bash\n""")
+        output.write(f"""{user_final}:admin:{random.randint(1111,9999)}:{random.randint(111,999)}:{nameFull},Brasil,{random.randint(111,999)}-{random.randint(111,999)}:/home/{user_final}:/bin/bash\n""")
 
 if __name__ == "__main__":
     create_user(nom)
